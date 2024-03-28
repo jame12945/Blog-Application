a083e63971199a33a1a48efbe82b25b67b265f1a
jame12945 <jame12945@gmail.com>
2024-03-28 17:07:35 +0700

    working process 15%

---
+++ blog_app/lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://xodtobfwhknedixflmjv.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvZHRvYmZ3aGtuZWRpeGZsbWp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE2MTg4MjAsImV4cCI6MjAyNzE5NDgyMH0.qKvxgoA5BXqkY9X_d5amz4fK0yLAPLLAtPaMH4jYdtU';
+}
--- blog_app/lib/main.dart
+++ blog_app/lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:blog_app/core/secrets/app_secrets.dart';
 import 'package:blog_app/core/theme/theme.dart';
 import 'package:blog_app/featutes/auth/presentation/pages/login_page.dart';
 import 'package:blog_app/featutes/auth/presentation/pages/signup_page.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+      url: AppSecrets.supabaseUrl, anonKey: AppSecrets.supabaseAnonKey);
   runApp(const MyApp());
 }
 
